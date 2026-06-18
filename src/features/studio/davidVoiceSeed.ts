4fdc9527bba2fd55382aee6e4a62ce7ef1a09503
Qing Lan <qinglan674@gmail.com>
2026-06-18 08:41:02 -0700

    Set David default voice ID for studio preview

--- src/features/studio/davidVoiceSeed.ts
+++ src/features/studio/davidVoiceSeed.ts
@@ -3,8 +3,7 @@ import type { VoiceScriptOption } from "./VoiceContextPreview";
 
 export const DAVID_VOICE_ID =
   import.meta.env.VITE_DAVID_VOICE_ID ||
-  import.meta.env.VITE_WALT_VOICE_ID ||
-  "default--z5zasdfwci5ofrt-gmsjw__walt";
+  "default--z5zasdfwci5ofrt-gmsjw__test";
 export const DAVID_VOICE_STUDIO_EVENT_ID = "evt-david-west-all-star";
 
 export const DAVID_VOICE_STUDIO_DRAFT = {
